6ecb9a50d09ed3db72d3e82b12110c730a232920
Dwyer Deighan <dwyerdei@buffalo.edu>
2025-08-19 17:24:25 -0400

    added git alternative to RC_file_installer.sh

---
+++ setup_RC_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# git alternative to the RC_file_installer.sh
+rm ~/.vimrc ~/.inputrc ~/.condarc ~/.bash_aliases ~/.Rprofile
+\ln .vimrc ~
+\ln .inputrc ~
+\ln .condarc ~
+\ln .bash_aliases ~
+\ln .Rprofile ~
