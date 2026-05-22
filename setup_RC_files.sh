af19b221802036e0cfe6f6bcde5de858788d4171
Dwyer Deighan <dwyerdei@buffalo.edu>
2026-05-21 21:42:59 -0400

    major update to setup_RC_files.sh that fixes important bugs, auto-updates, can handle mac or linux setups, handles most edge-cases, and creates elegant simple/short mac links
    Lastly it lets us delete the redundant ~/GoogeDrive folder

--- setup_RC_files.sh
+++ setup_RC_files.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-# git alternative to the RC_file_installer.sh
-rm ~/.vimrc ~/.inputrc ~/.condarc ~/.bash_aliases ~/.Rprofile
-\ln .vimrc ~
-\ln .inputrc ~
-\ln .condarc ~
-\ln .bash_aliases ~
-\ln .Rprofile ~
-source ~/.bash_aliases
+# Git alternative to the RC_file_installer.sh
+# * NEW INSTALL: git clone https://github.com/profPlum/RC_files.git ~/.RC_files; . ~/.RC_files/setup_RC_files.sh
+# * auto-updates once installed
+# * installs repo into ~/.RC_files
+# * can be used on mac to setup RC file links (but still requires manual mac.bashrc link)
+
+# This is the directory of this bash script!
+source_dir=$(dirname "$(realpath $BASH_SOURCE)") # Verified to work: 5/21/26
+source_dir_relative="${source_dir##$HOME/}" # (relative to home) for simpler/less verbose links
+
+# simplify relative path EVEN FURTHER (create ~/.RC_files sym link if possible for short readable paths)
+[[ ! -e ~/.RC_files ]] && \ln -s "$source_dir_relative" ~/.RC_files
+[[ "$(realpath ~/.RC_files)" == "$source_dir" ]] && source_dir_relative=".RC_files"
+
+# Technically: assumes no space in RC file names (but easy to add more!)
+RC_file_list=".vimrc .inputrc .condarc .bash_aliases .Rprofile"
+
+# NOTE: hard links won't work because file indentity changes due to Git and/or GDrive
+for RC_file in $RC_file_list; do
+    echo installing: $RC_file
+    rm ~/$RC_file
+    \ln -s "$source_dir_relative/$RC_file" ~
+done
+source ~/.bash_aliases # import bash aliases for immediate use
+
+if [[ "$(uname)" == Darwin ]]; then
+    echo mac detected! installing mac.bashrc...
+    rm ~/.bashrc
+    \ln -s "$source_dir_relative/mac.bashrc" ~/.bashrc
+fi
+
+# delete any previous auto-update line(s)
+\sed '/echo Updating \.RC_files.*/d' ~/.bashrc > /tmp/.bashrc_clean
+cat /tmp/.bashrc_clean > ~/.bashrc
+
+echo installing auto-update to .bashrc...
+echo "echo Updating .RC_files...; (\\cd '$source_dir'; git stash &> /dev/null; git pull &> /dev/null; git stash apply) # keep RC_files up to date" >> ~/.bashrc
